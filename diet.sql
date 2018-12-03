528a10705d92c543ab788fd0fff2e12aaacebf7d
00Emily00 <emyzu97@hotmail.com>
2018-12-03 11:04:54 -0700

    initial version

--- diet.sql
+++ diet.sql
@@ -3,6 +3,9 @@ CREATE TABLE login (
     , username VARCHAR(255) UNIQUE NOT NULL
     , password VARCHAR(255) NOT NULL
 );
+
+INSERT INTO login(username, password) VALUES('emily', 'barrera');
+
 CREATE TABLE ingredient_names (
     id SERIAL PRIMARY KEY NOT NULL
     , ingredients VARCHAR(500) NOT NULL
@@ -17,7 +20,3 @@ CREATE TABLE recipes (
     , ingredients_id SMALLINT REFERENCES ingredient_names(id)
     , instructions_id SMALLINT REFERENCES recipe_instructions(id)
 );
-
-CREATE USER loginUser WITH PASSWORD 'user';
-GRANT SELECT, INSERT, UPDATE ON login TO loginUser;
-GRANT USAGE, SELECT ON SEQUENCE login_id_seq TO loginUser;
--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 var express = require("express");
 var app = express();
 
+const { Pool } = require("pg");
+//connection needed to connect to the database --the server, etc
+const connectionString = process.env.DATABASE_URL || "postgres://:emily@localhost:5432/dietplanproject";
+const pool = new Pool({connectionString: connectionString});
 app.set("port", (process.env.PORT || 5000));
 
 app.get("/login" , getLogin); //endpoint
 app.get("/diet", getDiet);
 //app.get("/dietInformation"); //do i need this if single page website?
 //app.get("/recipes")
-
 
 app.listen(app.get("port"), function() {
     console.log("Now listening for connections on port: ", app.get("port"));
@@ -20,3 +23,7 @@ function getLogin(req, res) {
 function getDiet(req, res) {
     console.log("getting diet information");
 }
+
+CREATE USER loginUser WITH PASSWORD 'user';
+GRANT SELECT, INSERT, UPDATE ON login TO loginUser;
+GRANT USAGE, SELECT ON SEQUENCE login_id_seq TO loginUser;
