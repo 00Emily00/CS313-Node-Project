a204a619d6a08200a53fc55e75d9320e1abad078
00Emily00 <emyzu97@hotmail.com>
2018-12-05 13:01:39 -0700

    initial version

--- public/main-client.js
+++ public/main-client.js
@@ -1,4 +1,8 @@
 function getInformation() {
     alert("woohoo you did step 2");
     console.log("Getting diet information");
+    
+    //updating the DOM
+    var info = $("#dietFacts").val();
+    console.log("Info" +info);
 }
