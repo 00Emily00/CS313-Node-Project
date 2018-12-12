99f81219932450319766adfe1e1e4f1bbeaeea32
00Emily00 <emyzu97@hotmail.com>
2018-12-11 19:19:11 -0700

    Project 2 Node single page

--- public/main-client.js
+++ public/main-client.js
@@ -66,7 +66,7 @@ function postIngredients() {
     alert("postIngredients function was called");
     
     let add = "";
-    add += "<input type='text' name='ingredient'>";
+    add += "Ingredient<input type='text' name='ingredient'> Quantity<input type='text' name='ingredient'>Calories<input type='text' name='ingredient'>Food Group<input type='text' name='ingredient'>";
     
     document.getElementById("content2").innerHTML = add;
    
